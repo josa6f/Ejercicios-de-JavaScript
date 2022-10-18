10ae6e7d04d70286e2ef19f78fed4771009e6cba
Josafath Fuentes <josafm8@gmail.com>
2022-10-18 11:55:03 -0500

    Ejercicio 16
    Se resolvió el ejercicio 16

--- JS/16.js
+++ JS/16.js
@@ -1,3 +1,15 @@
 "use strict"
 
-//
+/*Que ingrese el usuario un numero y que el programa realice la suma desde 1 hasta N,
+donde N es el numero que digito el usuario*/
+
+var numero = new Number;
+var i = new Number;
+var suma = 0;
+
+numero = Number(prompt("Digite un numero"));
+
+for (let i = 0; i < numero; i++) {
+    suma = suma + i;
+}
+alert("La sumas es: " + suma );
