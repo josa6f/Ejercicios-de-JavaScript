577a7508217603e48a896f8f7c7fe0c2868188f6
Josafath Fuentes <josafm8@gmail.com>
2022-10-19 11:03:01 -0500

    Ejercicio 17
    resultado del ejercicio 17

--- JS/17.js
+++ JS/17.js
@@ -2,3 +2,21 @@
 
 /*Se desea calcular independientemente la suma de los numeros pares e impares comprendidos entre 1 y 50*/
 
+var numeroPar = new Number;
+var numeroImpar = new Number;
+var i = new Number;
+
+numeroPar = 0;
+numeroImpar = 0;
+
+for ( i = 2; i < 49; i++){
+    if (i % 2 == 0) {
+        numeroPar = numeroPar + i;
+        
+    }else{
+        numeroImpar = numeroImpar + i;
+    }
+}
+
+alert("La suma de los numeros pares es:" + numeroPar);
+alert("La suma de los numeros pares es:" + numeroImpar);
